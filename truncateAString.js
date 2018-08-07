60b1f57c96a2a33af503be9624d72f73fd598f35
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-08-06 19:03:26 -0700

    Create truncateAString.js

---
+++ truncateAString.js
@@ -0,0 +1,11 @@
+/* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending. */
+
+function truncateString(str, num) {
+  if(str.length > num) {
+    let truncatedStr = str.slice(0, num);
+    return truncatedStr + "..."
+  } else {
+    return str;
+  }
+}
