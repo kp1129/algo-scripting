b03c3c093846c0f963a47e0555f664c153b6e2aa
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-08-11 01:22:33 -0700

    Create seek-and-destroy.js

---
+++ seek-and-destroy.js
@@ -0,0 +1,9 @@
+/*You will be provided with an initial array (the first argument in the destroyer function), 
+followed by one or more arguments. 
+
+Remove all elements from the initial array that are of the same value as these arguments.*/
+
+function destroyer(arr) {
+  let targets = Array.from(arguments).slice(1, );
+  return arr.filter((x) => !targets.includes(x));
+}
