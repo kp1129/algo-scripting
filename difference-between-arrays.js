1d965d81fb169e7191f8e07315dfdac3d24f4db9
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-08-11 01:13:17 -0700

    Create difference-between-arrays.js

---
+++ difference-between-arrays.js
@@ -0,0 +1,7 @@
+/* Compare two arrays and return a new array with any items only found in one
+of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.*/
+
+function diffArray(arr1, arr2) {
+  return arr1.filter((x) => !arr2.includes(x)).concat(arr2.filter((x) => !arr1.includes(x)));
+}
